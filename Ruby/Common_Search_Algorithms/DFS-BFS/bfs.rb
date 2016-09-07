64c5d443d296e2c1b3d6ad72653b6a9268eddeb9
Michael <mtevans90@gmail.com>
2016-09-07 12:22:17 -0700

    adding more algorithms

--- Ruby/Common_Search_Algorithms/DFS-BFS/bfs.rb
+++ Ruby/Common_Search_Algorithms/DFS-BFS/bfs.rb
@@ -1,1 +1,15 @@
 
+def bfs(target, &prc)
+
+  prc ||= Proc.new{|node| node.value === target}
+  stack = [self]
+
+  until stack.empty?
+    node = stack.shift
+    return node if prc.call(node)
+    stack.concat(node.children)
+  end
+
+
+  nil
+end
--- Ruby/Common_Search_Algorithms/DFS-BFS/dfs.rb
+++ Ruby/Common_Search_Algorithms/DFS-BFS/dfs.rb
@@ -1,1 +1,11 @@
 
+def dfs(target, &prc)
+
+  prc ||= Proc.new{|node| node.value === target}
+  return self if prc.call(self)
+  children.each do |child|
+    result = child.dfs(target, &prc)
+    return result unless result.nil?
+  end
+  nil
+end
