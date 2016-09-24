70893fc8eba1b0ae4429ed7afaca2a3bef62060f
Michael <mtevans90@gmail.com>
2016-09-23 19:19:32 -0700

    adding more algorithms

---
+++ JavaScript/Common_Algorithms/insertionSort/insertionSortAns.js
@@ -0,0 +1,17 @@
+function insertionSort(arr){
+  for (var i = 0; i < arr.length; i++) {
+    var keepGoing = true
+    for (var j = i - 1; j >= 0 && keepGoing; j--) {
+      var item = arr[j + 1]
+      if(item >= arr[j]){
+        keepGoing = false
+      }
+      if(item < arr[j]){
+        arr[j+1] = arr[j]
+        arr[j] = item
+      }
+    }
+  }
+  return arr
+}
+ console.log(insertionSort([24,123,4,1,6,3,91]))
